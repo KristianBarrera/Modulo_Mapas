5e5f42264e60833f2ce64f0e64001679def5688b
MarioAlbertoCuevas <101274901+MarioAlbertoCuevas@users.noreply.github.com>
2022-05-13 14:24:25 -0500

    cambio debounce
    creacion de la primer instruccion para nuestro debounce manual

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,} from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
-export class SearchBarComponent implements OnInit {
+export class SearchBarComponent {
 
   constructor() { }
+  onQueryChanged( query: string = ' ' ) {
 
-  ngOnInit(): void {
+    console.log(query)
   }
 
 }
