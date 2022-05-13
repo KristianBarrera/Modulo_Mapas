f5e96790b0a9161c9e013fd364377e863c0fed4b
CarlosOmarCruzGarcia <101274898+CarlosOmarCruzGarcia@users.noreply.github.com>
2022-05-12 21:38:57 -0500

    Update map.service.ts
    Se crean dos propiedades para establecerlo asi como tambien se crea un metodo que recibira cordenadas usando un objeto que tenga longitud y latitud, asi como tambien se ace una cuestion para madar a llamar instrcciones que no tiene.

--- src/app/maps/services/map.service.ts
+++ src/app/maps/services/map.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { LngLatLike, Map } from 'mapbox-gl';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
 
-  constructor() { }
+  private map?: Map;
+
+  get isMapReady(){
+    return !!this.map;
+  }
+
+  setMap( map: Map ){
+    this.map = map;
+  }
+
+  flyto( coords: LngLatLike ){
+    if ( !this.isMapReady) throw Error ('El mapa no esta inicializado' )    
+  this.map?.flyTo({
+    zoom:14,
+    center: coords
+  });
+  }
 }
+
+
+  
