861f16fe75170e604251c465fae27741ce5b0123
AngelNV <92199054+AngelNV@users.noreply.github.com>
2022-05-25 17:14:29 -0500

    Debounce Manual
    En esta parte se realizo cambio en del Debounce para hacer busquedas de formas manuales

---
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -0,0 +1,24 @@
+import { Component, Query,} from '@angular/core';
+import { timeout } from 'rxjs';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent {
+
+  private debounceTimer?: NodeJS.Timeout;
+
+  constructor() { }
+  onQueryChanged( query: string = " " ) {
+    if (this.debounceTimer)clearTimeout(this.debounceTimer);
+    this.debounceTimer=setTimeout(()=>{
+      console.log('mandar este Query:',Query);
+    },350);
+
+
+    console.log(query)
+  }
+
+}
